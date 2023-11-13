fad18c61ffa24f675139a3d24035bc72266c281d
guzmanalejandra <guz20262@uvg.edu.gt>
2023-11-12 21:32:32 -0600

    DONE

--- consumer.py
+++ consumer.py
@@ -20,6 +20,37 @@ consumer.subscribe([topic_name])
 temperatures = deque(maxlen=50)  # Almacena las últimas 50 temperaturas
 humidities = deque(maxlen=50)    # Almacena las últimas 50 humedades
 
+def encode_sensor_data(temperature, humidity, wind_direction):
+    # Codificar la temperatura
+    temp_encoded = int((temperature / 100) * 16383)  # Escalar a 2^14 - 1
+
+    # Codificar la humedad
+    hum_encoded = humidity  # Ya es un valor entre 0-100
+
+    # Codificar la dirección del viento
+    wind_dir_map = {"N": 0, "NW": 1, "W": 2, "SW": 3, "S": 4, "SE": 5, "E": 6, "NE": 7}
+    wind_encoded = wind_dir_map[wind_direction]
+
+    # Combinar los datos codificados en un valor de 24 bits
+    combined = (temp_encoded << 10) | (hum_encoded << 3) | wind_encoded
+
+    # Convertir a 3 bytes
+    return combined.to_bytes(3, byteorder='big')
+
+def decode_sensor_data(encoded_bytes):
+    combined = int.from_bytes(encoded_bytes, byteorder='big')
+
+    # Extraer cada valor
+    temperature = ((combined >> 10) & 0x3FFF) / 16383 * 100
+    humidity = (combined >> 3) & 0x7F
+    wind_direction = combined & 0x07
+
+    # Mapeo inverso para la dirección del viento
+    wind_dir_map = {0: "N", 1: "NW", 2: "W", 3: "SW", 4: "S", 5: "SE", 6: "E", 7: "NE"}
+    wind_direction = wind_dir_map[wind_direction]
+
+    return temperature, humidity, wind_direction
+
 # Función para procesar mensajes y actualizar gráficos
 def consume_and_plot(consumer):
     plt.ion()
@@ -30,10 +61,11 @@ def consume_and_plot(consumer):
         if msg is None or msg.error():
             continue
 
-        # Extraer datos del mensaje
-        data = json.loads(msg.value().decode('utf-8'))
-        temperatures.append(data['temperature'])
-        humidities.append(data['humidity'])
+        # Extraer y decodificar datos del mensaje
+        encoded_data = msg.value()
+        temperature, humidity, wind_direction = decode_sensor_data(encoded_data)
+        temperatures.append(temperature)
+        humidities.append(humidity)
 
         # Actualizar gráficos
         ax1.clear()
--- producer.py
+++ producer.py
@@ -11,24 +11,52 @@ kafka_config = {
 # Crear el productor de Kafka
 producer = Producer(kafka_config)
 
+def encode_sensor_data(temperature, humidity, wind_direction):
+    # Codificar la temperatura
+    temp_encoded = int((temperature / 100) * 16383)  # Escalar a 2^14 - 1
+
+    # Codificar la humedad
+    hum_encoded = humidity  # Ya es un valor entre 0-100
+
+    # Codificar la dirección del viento
+    wind_dir_map = {"N": 0, "NW": 1, "W": 2, "SW": 3, "S": 4, "SE": 5, "E": 6, "NE": 7}
+    wind_encoded = wind_dir_map[wind_direction]
+
+    # Combinar los datos codificados en un valor de 24 bits
+    combined = (temp_encoded << 10) | (hum_encoded << 3) | wind_encoded
+
+    # Convertir a 3 bytes
+    return combined.to_bytes(3, byteorder='big')
+
+def decode_sensor_data(encoded_bytes):
+    combined = int.from_bytes(encoded_bytes, byteorder='big')
+
+    # Extraer cada valor
+    temperature = ((combined >> 10) & 0x3FFF) / 16383 * 100
+    humidity = (combined >> 3) & 0x7F
+    wind_direction = combined & 0x07
+
+    # Mapeo inverso para la dirección del viento
+    wind_dir_map = {0: "N", 1: "NW", 2: "W", 3: "SW", 4: "S", 5: "SE", 6: "E", 7: "NE"}
+    wind_direction = wind_dir_map[wind_direction]
+
+    return temperature, humidity, wind_direction
+
+
 # Función para simular datos de sensores meteorológicos
 def simulate_sensor_data():
     temperature = random.uniform(0, 100)  # Temperatura en grados Celsius
     humidity = random.randint(0, 100)     # Humedad en porcentaje
     wind_directions = ["N", "NW", "W", "SW", "S", "SE", "E", "NE"]
     wind_direction = random.choice(wind_directions)  # Dirección del viento
-    return {
-        "temperature": round(temperature, 2),
-        "humidity": humidity,
-        "wind_direction": wind_direction
-    }
+    return temperature, humidity, wind_direction
 
 # Función para enviar datos de sensores a Kafka
 def produce_sensor_data(producer, topic_name):
-    sensor_data = simulate_sensor_data()
-    data_str = json.dumps(sensor_data)
-    producer.produce(topic_name, value=data_str)
-    print(f"Enviado al topic {topic_name}: {data_str}")
+    temperature, humidity, wind_direction = simulate_sensor_data()
+    encoded_data = encode_sensor_data(temperature, humidity, wind_direction)
+    producer.produce(topic_name, value=encoded_data)
+    print(f"Enviado al topic {topic_name}: Temperatura: {temperature}, Humedad: {humidity}, Dirección del Viento: {wind_direction}")
 
 topic_name = '20262_20009'  # Carnets integrante
 
